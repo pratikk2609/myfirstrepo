fe096ee6c5a4f7d5e7a924953acd9d247da655db
pratikk2609 <pratikpawar2609@gmail.com>
2022-09-08 21:56:18 +0530

    matrixadd

--- second.c
+++ second.c
@@ -1,5 +1,52 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello pravin pawar");
-} 
+    int a[2][3], b[2][3], c[2][3], i, j;
+    printf("Enter first matrix:\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter second matrix:\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+printf("first matrix is:\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("second matrix is:\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d\t", b[i][j]);
+        }
+        printf("\n");
+    }
+    printf("The sum matrix is:\n");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            c[i][j]=a[i][j]+b[i][j];
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+}
