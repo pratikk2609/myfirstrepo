1ab7fc6e2946cde658a38f6a187cc14ca3313ab0
pratikk2609 <pratikpawar2609@gmail.com>
2022-09-26 15:17:19 +0530

    npr program

---
+++ npr.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int factorial(int n)
+{
+    int i, fact=1;
+    for(i=n; i>=1; i--)
+    {
+        fact=fact*i;
+    }
+    return fact;
+}
+
+int calculatenpr(int n, int r)
+{
+    int nfact, nminusrfact , finalnpr;
+    nfact=factorial(n);
+    nminusrfact=factorial(n-r);
+    //printf("nfact=%d\n, nminusrfact=%d\n", nfact, nminusrfact);
+    finalnpr=nfact/nminusrfact;
+    return finalnpr;
+}
+int main() 
+{
+    int n, p, npr, fact, i, r;
+    printf("enter a number n:");
+    scanf("%d", &n);
+    printf("enter a number r:");
+    scanf("%d", &r);
+    npr = calculatenpr(n, r);
+    printf("value of %dp%d=%d\n", n, r, npr);
+    
+}
