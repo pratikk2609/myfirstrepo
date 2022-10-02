e94590118c35f357581247c153f42682348f594d
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-02 15:34:16 +0530

    array

--- averagemarks.c
+++ averagemarks.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    float a,b,c;
+    float a, b, c;
     float average;
 
     printf("enter subject1 marks:");
@@ -10,18 +10,18 @@ int main()
     scanf("%f", &b);
     printf("enter subject3 marks:");
     scanf("%f", &c);
-    average=(a+b+c)/3;
+    average = (a + b + c) / 3;
     printf("average is %f", average);
 
-    if(average>=75)
+    if (average >= 75)
     {
         printf("\ndistinction");
     }
-    else if(average>=60 && average<=74.99)
+    else if (average >= 60 && average <= 74.99)
     {
         printf("\nfirst class");
     }
-    else if(average<60)
+    else if (average < 60)
     {
         printf("\naverage");
     }
@@ -30,5 +30,4 @@ int main()
         printf("");
     }
     return 0;
-
 }
