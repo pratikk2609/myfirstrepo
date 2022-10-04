9f5de263b98437a601ea5e6982c0af7d3439d678
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-04 09:47:28 +0530

    pattern 11

---
+++ 4oct2022/patern11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 5; j >= i; j--)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
