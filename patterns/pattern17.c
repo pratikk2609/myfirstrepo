2fdc698109e49cec2fc4036404a4687fd928a4a0
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-04 13:49:31 +0530

    pattern 17

---
+++ patterns/pattern17.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n=5;
+    for(int i=n;i>=1;i--)
+    {
+        for(int j=n-1;j>=i;j--)
+        {
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
