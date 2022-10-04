e53dcb23734fbd26acfe0aacc223bdf01f7001bd
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-04 11:01:14 +0530

    ascii pattern

---
+++ 4oct2022/pattern11ascii.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=0;i<=4;i++)
+    {
+        for(j=4;j>=i;j--)
+        {
+            printf("%c", i+65);
+        }
+        printf("\n");
+    }
+}
