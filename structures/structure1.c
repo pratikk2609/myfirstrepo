6e20c850ddfa384c7f1c748ccd540edbcdf32eb1
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-05 13:53:18 +0530

    declaration of structure

---
+++ structures/structure1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+struct Student{
+    int rollno;
+    float marks;
+    char name[20];
+};
+
+void main()
+{
+    int a;
+    struct Student s1, s2;
+}
+//will store data of students
