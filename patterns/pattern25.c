7d03c17dc71932be4837c6dcf555999a62f38109
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-05 09:05:30 +0530

    pattern with fist, last and side rows of *

---
+++ patterns/pattern25.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n = 5;
+    int i, j;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            if (i == 1 || i == n || j == 1 || j == n)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+}
