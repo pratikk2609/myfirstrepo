92942ad57bc675173a9bc8ebed13c91391d2608c
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-06 10:48:05 +0530

    typedef in struct

--- structures/structure5.c
+++ structures/structure5.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
-typedef struct student
+typedef struct student // typdef-> it is used to create additional name(alias) for another data type.
 {
     int rollno;
     char name[10];
     float marks;
 
-}student;
+} student;
 
 void main()
 {
-    student s={1,"\npratik", 85};
+    student s = {1, "pratik", 85};
     printf("%d%s%f", s.rollno, s.name, s.marks);
 }
