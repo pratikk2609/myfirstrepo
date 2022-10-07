506a6ce4075ea28fd5c82cbb89fc4a4cadf05f65
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-07 15:20:16 +0530

    concatenation

---
+++ Strings/string4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int l1,l2,i;
+    char s1[30]="pratik";
+    char s2[]="pawar";
+    //strcat(s1,s2);
+
+    l1=strlen(s1);
+    l2=strlen(s2);
+    for(i=0;i<l2;i++)
+    {
+        s1[l1+i]=s2[i];
+    }
+    printf("string after concatenation is : %s\n", s1);
+    puts(s2);
+}
