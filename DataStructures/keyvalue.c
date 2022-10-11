2c578dc65bd1e38a4d1a5828b5cc94aa423e516b
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-11 12:40:44 +0530

    keyvalue

---
+++ DataStructures/keyvalue.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct charset
+{
+    char s;
+    int i;
+};
+
+void keyValue(char *s, int *i)
+{
+    scanf("%c %d", s, i);
+}
+
+int main()
+{
+    int j;
+    struct charset cs;
+    keyValue(&cs.s, &cs.i);
+    printf("%c %d", cs.s, cs.i); // to print one character and one code associated with it
+}
