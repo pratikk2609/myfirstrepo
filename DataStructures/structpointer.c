bb0f2f52c1138c7b117db2da60223de3a2c02933
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-11 12:55:14 +0530

    passing point. to struct

---
+++ DataStructures/structpointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+struct point
+{
+    int x;
+    int y;
+};
+
+void print(struct point * p)
+{
+    printf("%d %d\n", p->x, p->y);
+}
+
+int main()
+{
+    struct point p1 = {23, 45};
+    struct point p2 = {33, 99};
+    print(&p1); //calling by value
+    print(&p2);
+    return 0;
+}
