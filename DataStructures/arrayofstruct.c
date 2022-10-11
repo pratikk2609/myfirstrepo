8964ce157547cb9ad786dfc4e549f2932e4e4ff8
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-11 14:27:20 +0530

    array of struct

---
+++ DataStructures/arrayofstruct.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+struct point
+{
+    int x;
+    int y;
+
+};
+
+void print(struct point arr[])
+{
+    int i;
+    for(i=0;i<2;i++)
+    {
+        printf("%d %d\n", arr[i].x, arr[i].y);
+    }
+}
+
+int main()
+{
+    struct point arr[2]={{1,2}, {3,4}};
+    print(arr);
+    return 0;
+}
