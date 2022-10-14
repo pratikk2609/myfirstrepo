73a22f5aee148e35d4b1326fbe0f001a38a22e8e
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-14 13:25:25 +0530

    linked list traversal

---
+++ LinkedListNew/LinkedListTraversal.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void LinkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("element is %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+
+    head->data = 7;
+    head->next = second;
+
+    second->data = 14;
+    second->next = third;
+ 
+    third->data = 21;
+    third->next = fourth;
+
+    fourth->data = 28;
+    fourth->next = NULL;
+
+    LinkedListTraversal(head);
+
+    return 0;
+}
