0be0e29835d1b8a0c4be81dfa5d9be0fd01826f0
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-16 09:35:00 +0530

    remainder and quotient new

--- ProgramsNew/rem_and_quot.c
+++ ProgramsNew/rem_and_quot.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 
-int quotient(int a, int b)
-{
-    return a / b;
+int quotient(int a, int b){
+   return a / b;
 }
 
-int remainder(int a, int b)
-{
-    return a % b;
+int remainder(int a, int b){
+   return a % b;
 }
 
-int main()
-{
+int main(){
     int num1, num2, quot, rem;
-    printf("enter dividend:");
+    printf("Enter dividend: ");    
     scanf("%d", &num1);
-    printf("\nenter divisor:");
+    printf("Enter divisor: ");    
     scanf("%d", &num2);
-
+      
     quot = quotient(num1, num2);
-
+    
     rem = remainder(num1, num2);
-
-    printf("Quotient is %d\n", quot);
-    printf("Remainder is %d\n", rem);
-
+    printf("Quotient is: %d\n", quot);    
+    printf("Remainder is: %d", rem);
     return 0;
 }
