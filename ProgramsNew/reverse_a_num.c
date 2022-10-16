959943f9083fb5013aff43a8e6fde2dedcfd2d76
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-16 10:35:26 +0530

    reverse of num

--- ProgramsNew/reverse_a_num.c
+++ ProgramsNew/reverse_a_num.c
@@ -9,7 +9,7 @@ int main()
     while (num != 0)
     {
         reverse = (reverse * 10) + (num % 10);
-        num /= 10;
+        num /= 10;  //  Divide and Assingnment operator /= divides left operand with right and assign result to left operand
     }
     printf("reverse is %d", reverse);
 
