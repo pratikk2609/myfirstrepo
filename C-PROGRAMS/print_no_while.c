93a57baab1f157db77a1d3de3f75bbbae9c86d21
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-18 19:02:04 +0530

    print no using while loop

---
+++ C-PROGRAMS/print_no_while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int number;
+	number =1;
+	printf("Numbers from 1 to 10: \n");
+	while(number<=10)
+	{
+		printf("%d ",number);
+		number++;
+	}
+	
+	return 0;
+}
