8ae923f5190def2cf85770006606b03c928b9672
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-20 16:12:56 +0530

    accesing arr elem using pointers

---
+++ C-PROGRAMS/pointer_string.c
@@ -0,0 +1,18 @@
+// usng pointers to access the array elements
+// strings
+
+#include <stdio.h>
+int main()
+{
+    char name[] = "Pratik";
+    char *ptr;
+    ptr = name;
+    while (*ptr != '\0')
+    {
+        printf("%c", *ptr);
+        ptr++;
+    }
+    printf("\n");
+
+    return 0;
+}
