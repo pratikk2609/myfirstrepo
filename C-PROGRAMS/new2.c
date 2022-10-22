56132e1cf9730c9bf9ed3ea5ed5788f0a2a5708d
pratikk2609 <pratikpawar2609@gmail.com>
2022-10-22 19:29:58 +0530

    even odd

---
+++ C-PROGRAMS/new2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter any integer: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0)
+    {
+        printf("%d is Even Number", num);
+    }
+    else
+    {
+        printf("%d is Odd Number", num);
+    }
+
+    return 0;
+}
