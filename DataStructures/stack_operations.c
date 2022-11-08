9dafbddd1ad1635f5c9c886590f5f0eafc988879
pratikk2609 <pratikpawar2609@gmail.com>
2022-11-08 16:02:44 +0530

    stack operations

---
+++ DataStructures/stack_operations.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    struct stack *sp = (struct stack *) malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (int *)malloc(sp->size * sizeof(int));
+    printf("stack has been created succesfully\n");
+
+    printf("%d\n", isFull(sp));
+    printf("%d\n", isEmpty(sp));
+
+}   
