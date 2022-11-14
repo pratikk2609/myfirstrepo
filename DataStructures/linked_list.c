fa5f2ee21389fabc060e63acf430d4a2d5ed4468
pratikk2609 <pratikpawar2609@gmail.com>
2022-11-14 16:50:06 +0530

    linked list insertion

---
+++ DataStructures/linked_list.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void LinkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d  %d  %d \n", ptr, ptr->data, ptr->next);
+        ptr = ptr->next;
+    }
+}
+
+struct Node *insertAtFirst(struct Node *head, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->next = head;
+    ptr->data = data;
+    return ptr;
+}
+
+struct Node *insertAtIndex(struct Node *head, int data, int index)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *p = head;
+    int i = 0;
+
+    while (i != index - 1)
+    {
+        p = p->next;
+        i++;
+    }
+    ptr->data = data;
+    ptr->next = p->next;
+    p->next = ptr;
+    return head;
+}
+
+struct Node *insertAtEnd(struct Node *head, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *p = head;
+    ptr->data = data;
+
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    p->next = ptr;
+    ptr->next = NULL;
+    return head;
+}
+
+struct Node *insertAfterNode(struct Node *head, struct Node *prevNode,  int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data=data;
+
+    ptr->next=prevNode->next;
+    prevNode->next=ptr;
+
+    return head;
+}
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 7;
+    head->next = second;
+
+    second->data = 14;
+    second->next = third;
+
+    third->data = 21;
+    third->next = fourth;
+
+    fourth->data = 28;
+    fourth->next = NULL;
+
+    printf("Linked list before insertion:\n");
+    LinkedListTraversal(head);
+    // head = insertAtFirst(head, 77);
+    // head = insertAtIndex(head, 77, 3);
+    // head = insertAtEnd(head, 77);
+    head = insertAfterNode(head, second,  15);
+    printf("\nLinked list after insertion:\n");
+    LinkedListTraversal(head);
+
+    return 0;
+}
