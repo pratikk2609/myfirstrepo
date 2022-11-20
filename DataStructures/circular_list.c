edfe0a984022364f2ea394495876b15ed2247ab6
pratikk2609 <pratikpawar2609@gmail.com>
2022-11-20 23:27:59 +0530

    circular empty full

---
+++ DataStructures/circular_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct circularQueue
+{
+    int size;
+    int f;
+    int r;
+    int *arr;
+};
+
+int isEmpty(struct circularQueue *q)
+{
+    if (q->r == q->f)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isFull(struct circularQueue *q)
+{
+    if ((q->r + 1) % q->size == q->f)
+    {
+        return 1;
+    }
+    return 0;
+}
