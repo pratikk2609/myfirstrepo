e61d09226195ce4d0164ed51d8d8a1cc70d0e29e
pratikk2609 <pratikpawar2609@gmail.com>
2022-11-23 22:29:20 +0530

    queue using array

--- DataStructures/queue_using_arr.c
+++ DataStructures/queue_using_arr.c
@@ -27,9 +27,51 @@ int isFull(struct queue *q)
     return 0;
 }
 
+void enqueue(struct queue *q, int val)
+{
+    if (isFull(q))
+    {
+        printf("This Queue is full\n");
+    }
+    else
+    {
+        q->r++;
+        q->arr[q->r] = val;
+        printf("Enqued element: %d\n", val);
+    }
+}
+
+int dequeue(struct queue *q)
+{
+    int a = -1;
+    if (isEmpty(q))
+    {
+        printf("This Queue is empty\n");
+    }
+    else
+    {
+        q->f++;
+        a = q->arr[q->f];
+    }
+    return a;
+}
+
 int main()
 {
     struct queue q;
+    q.size = 4;
+    q.f = q.r = 0;
+    q.arr = (int *)malloc(q.size * sizeof(int));
+
+    enqueue(&q, 12);
+    enqueue(&q, 15);
+    enqueue(&q, 1);
+    printf("Dequeuing element %d\n", dequeue(&q));
+    printf("Dequeuing element %d\n", dequeue(&q));
+    printf("Dequeuing element %d\n", dequeue(&q));
+    enqueue(&q, 45);
+    enqueue(&q, 45);
+    enqueue(&q, 45);
 
     if (isEmpty(&q))
     {
@@ -39,4 +81,5 @@ int main()
     {
         printf("Queue is full\n");
     }
+    return 0;
 }
